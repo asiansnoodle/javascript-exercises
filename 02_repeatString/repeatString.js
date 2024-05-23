cba709d21bdf9fdd815f96d03b49d3cea440132f
Noodles <adrianalexis624@gmail.com>
2024-05-23 17:48:55 -0500

    Completed exercise 2
    Worked on looping x amout of times and appending string for each itteration.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(input, reps) {
 
+    if (reps < 0){
+        return "ERROR";
+    }
+    
+    let result = '';
+    for (let i=0; i<reps; i++){
+        result += input;
+    }
+    return result;
 };
 
 // Do not edit below this line
