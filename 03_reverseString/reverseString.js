640896b36bccd1e854b33c5d37d2a31b5851ceb7
Noodles <adrianalexis624@gmail.com>
2024-05-23 18:05:01 -0500

    Completed exercise 3
    Worked on looping backwards to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
 
+    let l = input.length;
+    let result = '';
+    for (let i=l-1; i>=0; i--){
+        result += input[i];
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
