563d09dce5b176690e80af503eea46b49237c7f4
Noodles <adrianalexis624@gmail.com>
2024-05-24 16:55:49 -0500

    completed exercise 5
    worked on looping to calculate a sum and comparing variable types to validate inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
 
+    if ((x < 0) || (y < 0)){
+        return "ERROR";
+    }
+    
+    if ((typeof x !== typeof 1) || (typeof y !== typeof 1)){
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (x < y){
+        for (let i=x; i<=y; i++){
+            sum += i;
+        }
+    }
+    else {
+        for (let i=y; i<=x; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
