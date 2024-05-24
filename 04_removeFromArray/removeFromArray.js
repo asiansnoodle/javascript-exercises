b69b0b94576aadf88972f57adf0ab9d48876125e
Noodles <adrianalexis624@gmail.com>
2024-05-24 16:33:57 -0500

    Completed exercise 4
    Practiced using 'Rest Paramters' and removing elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    let result = [];
+
+    for (let i=0; i<arr.length; i++){
+        if (args.includes(arr[i]) == false){
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
 
 };
+
+console.log(removeFromArray([1, 2, 3, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
