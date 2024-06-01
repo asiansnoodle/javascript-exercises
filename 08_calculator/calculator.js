d2dd1785458cd06b3016fb950e462c15711f531e
Noodles <adrianalexis624@gmail.com>
2024-06-01 14:27:51 -0500

    completed exercise 8
    worked on performing math calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i=0; i<arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let start = arr[0];
+  for (let i=1; i<arr.length; i++){
+    start = start * arr[i];
+  }
+  return start;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0) return 1;
+  if (x == 1) return 1;
+
+  return x * factorial(x - 1);
+
 };
 
 // Do not edit below this line
